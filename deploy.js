4195202944fd77920bb60b553b119bab1c436ec1
Jake Stout <sjake@vmware.com>
2021-11-27 12:02:55 -0500

    Lesson 2.59: setup infura rinkeby provider

--- deploy.js
+++ deploy.js
@@ -1,1 +1,11 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const {interface, bytecode} = require('./compile')
+
+const provider = new HDWalletProvider(
+    'walk thought bachelor beach salute label aim loan junior coffee inhale worry',
+    'https://rinkeby.infura.io/v3/f54678ac0b17468ea21735519dd1c4ad'
+)
+
+const web3 = new Web3(provider)
