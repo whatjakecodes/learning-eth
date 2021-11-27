95555e013968514f2a2829705508bf50584501f4
Jake Stout <sjake@vmware.com>
2021-11-27 12:18:18 -0500

    60: deployed contract to rinkeby

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 // deploy code will go here
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 const Web3 = require('web3')
-const {interface, bytecode} = require('./compile')
+const {interface: abInterface, bytecode} = require('./compile')
 
 const provider = new HDWalletProvider(
     'walk thought bachelor beach salute label aim loan junior coffee inhale worry',
@@ -9,3 +9,17 @@ const provider = new HDWalletProvider(
 )
 
 const web3 = new Web3(provider)
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts()
+    console.log("Attempting to deploy from account ", accounts[0])
+
+    const contractSendMethod = new web3.eth.Contract(JSON.parse(abInterface))
+        .deploy({data: bytecode, arguments: ["hello, rinkeby!"]})
+
+    const result = await contractSendMethod.send({from: accounts[0], gas: 1000000})
+    console.log('Contract deployed to ', result.options.address)
+
+    // prevent a hanging deployment
+    provider.engine.stop()
+}
+deploy()
