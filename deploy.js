ad923aeee246b3d109300f617c5716b055e7460a
Jake Stout <kyjstout@gmail.com>
2021-12-29 15:38:57 -0500

    Fix deploy

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 // deploy code will go here
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 const Web3 = require('web3')
-const {abi,evm} = require('./compile-fn')
+const {abi, evm} = require('./compile')
 
 const provider = new HDWalletProvider(
     'walk thought bachelor beach salute label aim loan junior coffee inhale worry',
@@ -13,10 +13,11 @@ const deploy = async () => {
     const accounts = await web3.eth.getAccounts()
     console.log("Attempting to deploy from account ", accounts[0])
 
-    const contractSendMethod = new web3.eth.Contract(abi)
-        .deploy({data: evm.bytecode.object, arguments: ["hello, rinkeby!"]})
+    const result = await new web3.eth.Contract(abi)
+        .deploy({data: evm.bytecode.object})
+        .send({from: accounts[0], gas: 1000000})
 
-    const result = await contractSendMethod.send({from: accounts[0], gas: 1000000})
+    console.log(abi)
     console.log('Contract deployed to ', result.options.address)
 
     // prevent a hanging deployment
